ad9430fc5acdd4e995c7a68c01447820767d139e
Matthew Robinson <matt_robinson1@yahoo.com>
2020-02-06 18:36:07 +0000

    Automatically backed up by Learn

--- src/components/Band.js
+++ src/components/Band.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 class Band extends Component {
 
   render() {
+    console.log(this.props.band.id); 
     return(
       <div>
-        Band Component
+        Bands: 
+        <li> 
+          Name: {this.props.band.name}
+          <button onClick={() => this.props.delete(this.props.band)}>DELETE</button> 
+        </li> 
       </div>
     );
   }
